e76ebac0bb3546ff4b16a967996e6402a3f35da7
priyakapp <84661064+priyakapp@users.noreply.github.com>
2023-08-29 06:28:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/1238muj/THALAPATHY-FILTER-BOT.git /THALAPATHY-FILTER-BOT 
+  git clone https://github.com/priyakapp/Nobitareborn2-. /Nobitareborn2- 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Nobitareborn2-
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /Nobitareborn2-
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
